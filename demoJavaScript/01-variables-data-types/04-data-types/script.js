ea32a1f752b34cabf1505d263a5a305595e63989
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2024-01-06 10:56:29 +0700

    v1.0

---
+++ demoJavaScript/01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,38 @@
+// String
+const firstName = 'Quỳnh';
+
+// Number
+const age = 19;
+const temp = 98.9;
+
+// Boolean
+const hasKids = false;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 123446574837n;
+
+// Reference Types
+
+const numbers = [1, 2, 3, 4];
+
+const person = {
+  name: 'Quỳnh',
+};
+
+function sayHello() {
+  console.log('Hello');
+}
+
+const output = sayHello;
+
+console.log(output, typeof output);
